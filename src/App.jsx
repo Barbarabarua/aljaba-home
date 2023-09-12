350bb8e4fb796b6b8074318c731ac0d14bd461ce
Bárbara Barúa <Barb.barua@gmail.com>
2023-09-12 11:04:25 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import ItemListContainer from './components/ItemListContainer.jsx'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import ItemDetailContainer from './components/ItemDetailContainer'
+import ItemListContainer from './components/ItemListContainer'
 import Navbar from './components/Navbar.jsx'
+import Cart from './components/Cart.jsx'
+
 
 const App = () => {
-  return (
-    <>
-    <Navbar/>
-    <ItemListContainer greeting={"Bienvenido a la tienda online de Aljaba Home."}/>
-    </>
+
+
+  return ( 
+    <BrowserRouter>
+      <Navbar/>
+      
+      <Routes>
+        <Route exact patch='/' element={<ItemListContainer/>}/>
+        <Route exact patch='/cart' element={<Cart/>}/>
+        <Route exact patch='/categoria/:id' element={<ItemListContainer/>}/>
+        <Route exact patch='/item/:id' element={<ItemDetailContainer/>}/>
+
+    {/*<ItemListContainer greeting={"Bienvenido a la tienda online de Aljaba Home."}/>
+    */}
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
 
