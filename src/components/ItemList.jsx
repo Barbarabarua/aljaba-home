3fbbfe25da9cf5b7a8b6ab112192cc630ad78f55
Bárbara Barúa <Barb.barua@gmail.com>
2023-09-12 11:05:14 -0300

    Create ItemList.jsx

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Item from './Item';
+
+const ItemList = ({ productos }) => {
+  return (
+    <div>
+      {productos.map((p) => (
+        <Item producto={p} key={p.id} />
+        
+      ))}
+    </div>
+  );
+};
+
+export default ItemList;
+``
